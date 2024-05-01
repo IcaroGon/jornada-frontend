8868b05ef590f62ce38919e23c367ea2ed13a51a
IcaroGon <165965378+IcaroGon@users.noreply.github.com>
2024-05-01 00:06:39 +0000

    Fim Aula 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,14 @@
-import { useEffect, useState } from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css'
-import Card from './components/Card/Card'
+import ReadAll from './pages/ReadAll/ReadAll'
 
-function App() {
-  // const item1 = {
-  //   name: 'Rick Sanchez',
-  //   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-  //   tags: ['Status: Vivo', 'Espécie: Humana', 'Origem: Terra C-137']
-  // }
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <ReadAll />
+  }
+])
 
-  // const item2 = {
-  //   name: 'Morty Smith',
-  //   image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
-  //   tags: ['Status: Vivo']
-  // }
-
-  // const item3 = {
-  //   name: 'Summer Smith',
-  //   image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
-  //   tags: ['Espécie: Humana']
-  // }
-
-  // const itens = [item1, item2, item3]
-  const [itens, setItens] = useState([])
-
-  const apiUrl = 'https://rickandmortyapi.com/api/character/'
-
-  useEffect(function () {
-    async function carregarDados() {
-      const response = await fetch(apiUrl)
-  
-      const data = await response.json()
-
-      const results = data.results
-      setItens(results)
-    }
-
-    carregarDados()
-
-  }, [])
-
-
-  return (
-    <>
-      <div className="cards">
-        {itens.map(function (elemento) {
-          return <Card item={elemento} key={`card_${elemento.id}`} />
-        })}
-      </div>
-    </>
-  )
+export default function App() {
+  return <RouterProvider router={router} />
 }
-
-export default App
